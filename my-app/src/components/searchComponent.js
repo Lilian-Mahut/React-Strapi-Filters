6c04c95b058d93b8af59208788bbe5d464928e43
Lilian Mahut <lilian.mahut@pm.me>
2020-07-30 16:41:17 +0200

    fix searchCompo

--- my-app/src/components/searchComponent.js
+++ my-app/src/components/searchComponent.js
@@ -6,40 +6,41 @@ import "./searchComponent.scss"
 const searchComponent = () => {
     useEffect(() => {})
     return (
+        <div>
+            <p>Type d'événements</p>
+            <select name="typeOfEvent" id="type-event-select">
+                <option value="">--Choisir un type d'event</option>
+                <option value="sport">Sport</option>
+                <option value="boire">Boire</option>
+                <option value="manger">Manger</option>
+                <option value="voyage">Voyage</option>
+            </select>
+            <p>Choix événéments</p>
+            <div>
+                <input type="checkbox" id="sport" name="sport" />
+                <label for="sport">Sport</label>
+            </div>
+            <div>
+                <input type="checkbox" id="boire" name="boire" />
+                <label for="boire">Boire</label>
+            </div>
+            <div>
+                <input type="checkbox" id="manger" name="manger" />
+                <label for="manger">Manger</label>
+            </div>
+            <div>
+                <input type="checkbox" id="voyage" name="voyage" />
+                <label for="voyage">Voyage</label>
+            </div>
 
-        <p>Type d'événements</p>
-        <select name="typeOfEvent" id="type-event-select">
-            <option value="">--Choisir un type d'event</option>
-            <option value="sport">Sport</option>
-            <option value="boire">Boire</option>
-            <option value="manger">Manger</option>
-            <option value="voyage">Voyage</option>
-        </select>
-        <p>Choix événéments</p>
-        <div>
-            <input type="checkbox" id="sport" name="sport" />
-            <label for="sport">Sport</label>
+            <p>Places disponibles</p>
+            <select name="places" id="places">
+            <option value="all">Toutes</option>
+            <option value="twoMore">2 et +</option>
+            <option value="one">1</option>
+            <option value="empty">Vide</option>
+            </select>
         </div>
-        <div>
-            <input type="checkbox" id="boire" name="boire" />
-            <label for="boire">Boire</label>
-        </div>
-        <div>
-            <input type="checkbox" id="manger" name="manger" />
-            <label for="manger">Manger</label>
-        </div>
-        <div>
-            <input type="checkbox" id="voyage" name="voyage" />
-            <label for="voyage">Voyage</label>
-        </div>
-
-        <p>Places disponibles</p>
-        <select name="places" id="places">
-        <option value="all">Toutes</option>
-        <option value="twoMore">2 et +</option>
-        <option value="one">1</option>
-        <option value="empty">Vide</option>
-        </select>
     )
 }
                 
