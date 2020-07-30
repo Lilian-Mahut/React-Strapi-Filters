6671844b92d806a15b1246f071781be4ec32e2a1
Lilian Mahut <lilian.mahut@pm.me>
2020-07-30 16:44:28 +0200

    eventComponent

--- my-app/src/components/eventComponent.js
+++ my-app/src/components/eventComponent.js
@@ -1,1 +1,19 @@
+import React, { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+
+import "./eventComponent.scss"
+
+const eventComponent = () => {
+    useEffect(() => {})
+    return (
+        <div>
+            <div>Titre</div>
+            <div>Organisateur</div>
+            <div>Date</div>
+            <div>Participants</div>
+            <div>Description</div>
+        </div>
+    )
+}
+
 export default eventComponent
